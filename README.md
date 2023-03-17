4ec846468f248c9b5d9617de2e0265d29b0d633e
狐日泽 <shdocter@nya-wsl.com>
2023-03-18 04:14:47 +0800

    v2.0.0

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # random-number-py
- 
+
+基于python3.11.0 + thinker的简易随机数生成器
+
+
+
+
+
+
+
+![example](example.png)
--- main.py
+++ main.py
@@ -1,24 +1,83 @@
+import os
 import sys
 import random
+from tkinter import *
+import tkinter.messagebox as tkmb
 
-version = "1.1"
+version = "2.0.0"
 
-NumberList = None
+# global lb
+# global Number
+# global RangeMax
+# global RangeMin
+# global window
+# global CheckVar
 
-RangeChoiceMin = input("请输入生成范围的最小值：")
-RangeChoiceMax = input("请输入生成范围的最大值：")
-NumberChoice = input("请输入生成数量：")
-RepeatChoice = input("请选择生成方式（1=重复, 2=不重复）：")
+def clicked():
+    global listbox
+    global sc
+    global lb
+    global Compare
+    NumberList = ""
+    NumberText = int(Number.get())
+    RangeMinNumber = int(RangeMin.get())
+    RangeMaxNumber = int(RangeMax.get())
+    RangeMax1 = int(RangeMaxNumber) + int(1)
+    Compare = RangeMax1 - RangeMinNumber
+    print(f"最大值：{RangeMaxNumber} 最小值：{RangeMinNumber} 差异：{Compare}")
+    if int(NumberText) > int(Compare):
+        CompareError()
+    if CheckVar.get() == 1:
+        NumberList = [random.randint(int(RangeMinNumber),int(RangeMax1)) for _ in range(int(NumberText))]
+    elif CheckVar.get() == 0:
+        NumberList = random.sample(range(int(RangeMinNumber),int(RangeMax1)),int(NumberText))
+    lb = Label(window)
+    lb.pack(side=LEFT)
+    lb.configure(text = "结果：")
+    sc = Scrollbar(window)
+    sc.pack(side=LEFT, fill=Y)
+    listbox = Listbox(window, selectmode=EXTENDED, yscrollcommand=sc.set, height=5, width=10, borderwidth=0)
+    for i in NumberList:
+        listbox.insert(END, i)
+    listbox.pack(side=LEFT, fill=BOTH, )
+    sc.config(command=listbox.yview)
 
-RangeChoiceMax1 = int(RangeChoiceMax) + int(1)
+def reset():
+    listbox.destroy()
+    sc.destroy()
+    lb.destroy()
 
-if RepeatChoice == "1":
-    NumberList = [random.randint(int(RangeChoiceMin),int(RangeChoiceMax1)) for _ in range(int(NumberChoice))]
-elif RepeatChoice == "2":
-    NumberList = random.sample(range(int(RangeChoiceMin),int(RangeChoiceMax1)),int(NumberChoice))
-else:
-    input("参数错误，请重启程序并输入正确的参数")
-    sys.exit("参数错误")
+def CompareError():
+    tkmb.showwarning(title="警告", message="输入的生成数量大于最大值 " + str(Compare) + " ！")
 
-print(NumberList)
-input("按回车键结束")
+def get_var():
+    print(CheckVar.get())
+
+window = Tk()
+window.iconbitmap("Nya-WSL.ico")
+window.title(f"简易随机数生成器v{version} by.Nya-WSL")
+window.geometry('500x410')
+
+LblNumberCHoice = Label(window,text = "请输入生成数量")
+LblRangeChoiceMin = Label(window,text = "请输入生成范围的最小值")
+LblRangeChoiceMax = Label(window,text = "请输入生成范围的最大值")
+Number = Entry(window,width=10)
+RangeMin = Entry(window,width=10)
+RangeMax = Entry(window,width=10)
+
+LblNumberCHoice.pack()
+Number.pack()
+LblRangeChoiceMin.pack()
+RangeMin.pack()
+LblRangeChoiceMax.pack()
+RangeMax.pack()
+
+CheckVar = IntVar()
+CV = Checkbutton(window, text="允许生成重复的值", variable=CheckVar, onvalue=1, offvalue=0, height=1, command=get_var)
+CV.pack()
+
+btn = Button(window,fg="black",bg="white",text="开始",command=clicked)
+btn.pack()
+btnRestart = Button(window,fg="black",bg="white",text="重置结果",command=reset)
+btnRestart.pack(pady=5)
+window.mainloop()
